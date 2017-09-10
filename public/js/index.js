d8c262ce2068d84f8ea9b5f5837733953d30af08
root <Badrinath_Bhalkiker@gap.com>
2017-09-09 17:19:28 -0700

    New MEssage Emit

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,9 @@
 var socket = io();
 socket.on('connect',function (){
 	console.log('Connected to server');
-	socket.emit('createMessage',{
-		from:'Badri'
-	});
 });
 socket.on('disconnect',function() {
 	console.log('Disconnected from server');
-});
-
-socket.on('newEmail',function(email){
-	console.log('New Email',email);
 });
 
 socket.on('newMessage',function(message){
--- server/server.js
+++ server/server.js
@@ -14,22 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New User Connected');
 
-	socket.emit('newEmail',{
-		from:'bhalkikerbadri@gmail.com',
-		text: 'Hi'
-	});
-
-	socket.emit('newMessage',{
-		from: 'Anjali',
-		createdAt: 1222112
-	});
-
-	socket.on('createEmail',(newEmail) =>{
-		console.log('createEmail',newEmail);
-	});
-
 	socket.on('createMessage',(message) => {
 		console.log('createMessage',message);
+		io.emit('newMessage',{
+			from:message.from,
+			text:message.text,
+			createdAt:new Date().getTime()
+		});
 	});
 
 	socket.on('disconnect', () =>{
