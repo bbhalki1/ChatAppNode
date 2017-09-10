9b1cb977713eb0d3f8963694ec73b84f2b3c1fac
root <Badrinath_Bhalkiker@gap.com>
2017-09-09 17:02:21 -0700

    Adding new emitters and listeners

--- server/server.js
+++ server/server.js
@@ -14,10 +14,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New User Connected');
 
+	socket.emit('newEmail',{
+		from:'bhalkikerbadri@gmail.com',
+		text: 'Hi'
+	});
+
+	socket.emit('newMessage',{
+		from: 'Anjali',
+		createdAt: 1222112
+	});
+
+	socket.on('createEmail',(newEmail) =>{
+		console.log('createEmail',newEmail);
+	});
+
+	socket.on('createMessage',(message) => {
+		console.log('createMessage',message);
+	});
+
 	socket.on('disconnect', () =>{
 		console.log('User disconnected')
 	});
 });
+
+
 
 server.listen(port,() => {
 	console.log(`Server is up on ${port}`)
